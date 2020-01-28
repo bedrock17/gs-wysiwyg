b9ec40bb1d7922233f3b087c58b38074f63e8808
mobbing <ifthe1201@naver.com>
2020-01-28 23:07:31 +0900

    필요없는 패키지 제거 / 필요 컴포넌트 추가

---
+++ src/gs-wysiwyg-editor.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'Editor',
+
+	components: {
+	},
+
+	data: () => ({
+		//
+	}),
+});
+</script>
---
+++ src/gs-wysiwyg-toolbar.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'Toolbar',
+
+	components: {
+	},
+
+	data: () => ({
+		//
+	}),
+});
+</script>
