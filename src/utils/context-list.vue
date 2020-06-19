785797263ac98c927674c109baa226f7f52e5a58
Tree Some <youn@tree-some.dev>
2020-06-19 18:54:08 +0900

    Create component for [Contextmenu]

---
+++ src/utils/context-list.vue
@@ -0,0 +1,50 @@
+<template>
+	<v-menu
+		fixed
+		:width="w"
+		v-model="show"
+		:position-x="x"
+		:position-y="y"
+		offset-y>
+		<v-list dense>
+			<div v-for="(con, idx) in ctx">
+				<v-list-item
+					v-for="c in con"
+					@click="c.func($event, idx, con)"
+					:key="c.name"
+					style="font-size: 0.8rem; min-height:30px; padding-right:50px;">
+					{{ c.name }}
+				</v-list-item>
+				<v-divider class="my-2" v-if="idx < ctx.length-1"></v-divider>
+			</div>
+		</v-list>
+	</v-menu>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Model } from 'vue-property-decorator';
+
+export interface CtxListItem {
+	name: string;
+	func(evt: MouseEvent, idx: number, con: CtxListItem);
+}
+
+@Component
+export default class ContextList extends Vue {
+	@Prop(String) private width!: string;
+	@Prop(Array) private context!: any[];
+
+	private x: number = 0;
+	private y: number = 0;
+	private w: string = this.width;
+	private ctx: CtxListItem[][] = this.context;
+	private show: boolean = false;
+
+	public open(evt: MouseEvent): any {
+		this.x = evt.clientX;
+		this.y = evt.clientY;
+		this.show = true;
+		evt.preventDefault();
+	}
+}
+</script>
--- src/utils/table.vue
+++ src/utils/table.vue
@@ -1,22 +1,26 @@
 <template>
-	<div v-if="view">
+	<div>
 		<vue-good-table
 			class="gs-table-custom"
 			:columns="cols"
 			:sort-options="{ enabled: false }"
 			@on-cell-context="showContextMenu"
 			:rows="rows">
-			<template slot="table-column" slot-scope="props" @contextmenu="iii">
+			<template slot="table-column" slot-scope="props">
 				<div contenteditable style="outline: none;" @input="headerSync($event, props.column)">
 					test
 				</div>
 			</template>
 			<template slot="table-row" slot-scope="props">
-				<div contenteditable style="outline: none;" @input="iii(props)">
+				<div contenteditable style="outline: none;">
 					{{ props.formattedRow[props.column.field] }}
 				</div>
 			</template>
 		</vue-good-table>
+		<context-list
+			ref="context"
+			width="150px"
+			:context="ctx" />
 	</div>
 </template>
 
@@ -25,46 +29,62 @@
 </style>
 
 <script lang="ts">
-import Vue from 'vue';
 import { VueGoodTable } from 'vue-good-table';
 import 'vue-good-table/dist/vue-good-table.css';
+import ContextList from './context-list.vue';
+import { CtxListItem } from './context-list.vue';
+import { Vue, Component, Prop } from 'vue-property-decorator';
 
 interface VgtColumn {
 	label: string;
 	field: string;
 	filterable?: true;
+	typedef?: object;
 }
 
-export default Vue.extend({
-	name: 'GsTable',
+interface VgtCtxEvent {
+	row: (object | null);
+	column: VgtColumn;
+	rowIndex: number;
+	colIndex: number;
+	event: MouseEvent;
+}
+
+@Component({
 	components: {
 		VueGoodTable,
+		ContextList,
 	},
-	methods: {
-		headerSync(evt: Event, column: VgtColumn) {
-			const target: HTMLElement = evt.target as HTMLElement;
-			column.label = target.innerText;
-		},
-		showContextMenu(evt: MouseEvent) {
-			// empty
-			console.log(evt);
-		},
-		iii(props: any) {
-			console.log(props);
-		}
+	mounted() {
+		const comps = this as GsTable;
+		comps.rows = [
+			{ 'id': 1, 'field-1': 'Header' },
+		];
+		comps.cols = [
+			{
+				label: 'Hello',
+				field: 'field-1',
+			},
+		];
 	},
-	// tslint:disable-next-line
-	data() {
-		return {
-			view: true,
-			cols: [
-				{ label: 'Header', field: 'field-1' },
-				{ label: 'Two', field: 'field-2' },
-			],
-			rows: [
-				{ 'id': 1, 'field-1': 'Hello?', 'field-2': 'World!' },
-			],
-		};
-	},
-});
+})
+export default class GsTable extends Vue {
+	private cols: VgtColumn[] = [];
+	private rows: object[] = [];
+	private ctx: any[] = [
+		[
+			{ name: 'ttt', func: () => {} },
+		]
+	];
+
+	private headerSync(evt: Event, column: VgtColumn) {
+		const target: HTMLElement = evt.target as HTMLElement;
+		column.label = target.innerText;
+	}
+
+	private showContextMenu(data: VgtCtxEvent) {
+		const ctx: ContextList = this.$refs.context as ContextList;
+		ctx.open(data.event);
+	}
+}
 </script>
