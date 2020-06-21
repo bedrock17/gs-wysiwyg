948ef30e09d3ecbabd72ea9e771667a477a3832f
SHK <bedrook@gmail.com>
2020-06-21 12:40:24 +0900

    ICC 이벤트 핸들 코드 옮김

--- src/editor_script/gseditor.ts
+++ src/editor_script/gseditor.ts
@@ -24,6 +24,7 @@ export class GSEditor {
 	constructor(editorElement: any, icc: any) {
 		this.editorDivTagElement = editorElement;
 		this.ICC = icc;
+		this.init_icc();
 	}
 
 	public getHTML(): string {
@@ -67,5 +68,144 @@ export class GSEditor {
 			.apply(this.ICC['editor-icc'], [method].concat(args));
 	}
 
+	private init_icc() {
+		const hexToRgbString = (hex): string|null => {
+			const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+			if (result == null) {
+				return null;
+			}
+
+			const r = parseInt(result[1], 16).toString();
+			const g = parseInt(result[2], 16).toString();
+			const b = parseInt(result[3], 16).toString();
+
+			return `rgba(${r},${g},${b},1)`;
+		};
+
+		const EICC = this.ICC['editor-icc'];
+
+		EICC.on('erase', () => {
+			// tslint:disable-next-line
+			console.log("resive event erase");
+		});
+
+		EICC.on('undo', () => {
+			// tslint:disable-next-line
+			console.log("resive event undo");
+		});
+
+		EICC.on('redo', () => {
+			// tslint:disable-next-line
+			console.log("resive event redo");
+		});
+
+		EICC.on('change-align', (align: string) => {
+			// tslint:disable-next-line
+			console.log("resive event change-align", align);
+		});
+
+
+		EICC.on('text-front-color-change', (color: string) => {
+			// tslint:disable-next-line
+			console.log("resive event text-front-color-change", color);
+			const retVal = hexToRgbString(color);
+			if (retVal == null) {
+				return;
+			}
+			color = retVal;
+			document.execCommand('foreColor', true, color);
+		});
+
+		EICC.on('text-back-color-change', (color: string) => {
+			// tslint:disable-next-line
+			console.log("resive event text-back-color-change", color);
+			const retVal = hexToRgbString(color);
+			if (retVal == null) {
+				return;
+			}
+			color = retVal;
+			document.execCommand('backColor', true, color);
+		});
+
+		EICC.on('insert-picture', () => {
+			// tslint:disable-next-line
+			console.log("resive event insert-picture");
+		});
+
+		EICC.on('insert-table', () => {
+			// tslint:disable-next-line
+			console.log("resive event insert-table");
+		});
+
+		EICC.on('view-html', () => {
+			// tslint:disable-next-line
+			console.log("resive event view-html");
+		});
+
+		EICC.on('view-markdown', () => {
+			// tslint:disable-next-line
+			console.log("resive event view-markdown");
+		});
+
+		EICC.on('code-block', (code: string) => {
+			// tslint:disable-next-line
+			console.log("resive event code-block", code);
+		});
+
+		EICC.on('tag-change', (tag: string) => {
+			// tslint:disable-next-line
+			console.log("resive event tag-change", tag);
+		});
+
+		EICC.on('font-change', (font: string) => {
+			// tslint:disable-next-line
+			console.log("resive event font-change", font);
+		});
+
+		EICC.on('size-change', (size: string) => {
+			// tslint:disable-next-line
+			console.log("resive event size-change", size);
+		});
+
+		EICC.on('toggle-bold', (bold: boolean) => {
+			// tslint:disable-next-line
+			document.execCommand('bold');
+		});
+
+		EICC.on('toggle-italic', (italic: boolean) => {
+			// tslint:disable-next-line
+			console.log("resive event toggle-italic", italic);
+			document.execCommand('italic');
+		});
+
+		EICC.on('toggle-underline', (underline: boolean) => {
+			// tslint:disable-next-line
+			console.log("resive event toggle-underline", underline);
+			document.execCommand('underline');
+		});
+
+		EICC.on('toggle-strike', (strike: boolean) => {
+			// tslint:disable-next-line
+			console.log("resive event toggle-strike", strike);
+			document.execCommand('strikeThrough');
+		});
+
+		EICC.on('toggle-sub-super', (sub: boolean, sup: boolean) => {
+			// tslint:disable-next-line
+			console.log("resive event toggle-sub", sub, sup);
+			document.execCommand('superscript');
+		});
+
+		EICC.on('add-link', (link: string) => {
+			// tslint:disable-next-line
+			console.log("resive event add-link", link);
+		});
+
+		EICC.on('del-link', () => {
+			// tslint:disable-next-line
+			console.log("resive event del-link");
+		});
+
+	}
 
 }
